0083885e084df016c122d23817a1abac937f575c
Manolo Cabrera <viprestal@hotmail.com>
2023-08-30 20:13:04 +0200

    antes de contact

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,16 +2,19 @@
 import styles from './page.module.css'
 import Hero from 'public/hero.png'
 import Image from 'next/image'
+import Button from '@/components/button/button'
 
 export default function Home() {
   return (
-   <div className="">
-    <div className={styles.texts}>
-      <h1>Better design for your digital products.</h1>
-      <p>Turning your Idea into Reality, We bring together the teams from the global tech.</p>
+   <div  className={styles.container}>
+    <div className={styles.item}>
+      <h1  className={styles.title}>Better design for your digital products.</h1>
+      <p  className={styles.desc}>Turning your Idea into Reality, We bring together the teams from the global tech.</p>
+      <Button text="See Our Works" url="/portfolio" />
     </div>
-    <div className={styles.imgContainer}></div>
+    <div className={styles.item}>
     <Image src={Hero} alt="hero" className={styles.img}/>
+    </div>
    </div>
   )
 }
